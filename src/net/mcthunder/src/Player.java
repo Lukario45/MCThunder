2232bbedab277f0f1ba0ff7fe578f05d65ad5555
Lukario45 <connorcs@ptd.net>
2013-01-07 19:57:18 -0500

    New Player.cs

--- src/net/mcthunder/src/Player.java
+++ src/net/mcthunder/src/Player.java
@@ -1,12 +1,25 @@
 package net.mcthunder.src;
 /**
  * Player.java 
- * This Class will be for Holding the Player Info
+ * This Class will be for The Players
  * ~Lukario45
  */
 
 public class Player 
-
 {
-
+  private String PlayerNick;
+	public Player(String pn)
+	{
+		this.PlayerNick = pn;
+	}
+	//Getters
+	public String getPlayerNick()
+	{
+		return this.PlayerNick;
+	}
+	//To String
+	public String toString()
+	{
+		return PlayerNick;
+	}
 }
