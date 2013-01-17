f4cdfe7801503c5514cb1ddba9792377da861f4d
3picKillz <chris974@live.com>
2013-01-16 19:17:31 -0500

    Added Defualt Ranks

---
+++ src/net/mcthunder/src/Groups.java
@@ -0,0 +1,22 @@
+package net.mcthunder.src;
+
+public class Groups {
+	
+	public String RankName;
+	public int permissionlvl;
+	public String Color;
+	
+	public void groups(){
+		this.LoadRanks();
+	}
+	
+	void LoadRanks(){
+		
+	}
+	
+	public void addRank(String RankName, int permissionlvl, String Color)
+	{
+		
+	}
+	
+}
