b0d9fb1156ecdbe868cad47002436b245bd85581
Lukario45 <connorcs@ptd.net>
2014-08-10 17:19:24 -0400

    PacketLib

---
+++ src/main/java/packetlib/event/server/SessionAddedEvent.java
@@ -0,0 +1,42 @@
+package packetlib.event.server;
+
+import packetlib.Server;
+import packetlib.Session;
+
+/**
+ * Called when a session is added to the server.
+ */
+public class SessionAddedEvent implements ServerEvent {
+
+    private Server server;
+    private Session session;
+
+    public SessionAddedEvent(Server server, Session session) {
+        this.server = server;
+        this.session = session;
+    }
+
+    /**
+     * Gets the server involved in this event.
+     *
+     * @return The event's server.
+     */
+    public Server getServer() {
+        return this.server;
+    }
+
+    /**
+     * Gets the session involved in this event.
+     *
+     * @return The event's session.
+     */
+    public Session getSession() {
+        return this.session;
+    }
+
+    @Override
+    public void call(ServerListener listener) {
+        listener.sessionAdded(this);
+    }
+
+}
---
+++ src/main/java/packetlib/event/session/ConnectedEvent.java
@@ -0,0 +1,30 @@
+package packetlib.event.session;
+
+import packetlib.Session;
+
+/**
+ * Called when the session connects.
+ */
+public class ConnectedEvent implements SessionEvent {
+
+    private Session session;
+
+    public ConnectedEvent(Session session) {
+        this.session = session;
+    }
+
+    /**
+     * Gets the session involved in this event.
+     *
+     * @return The event's session.
+     */
+    public Session getSession() {
+        return this.session;
+    }
+
+    @Override
+    public void call(SessionListener listener) {
+        listener.connected(this);
+    }
+
+}
