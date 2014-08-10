9b72c287609588c5418c479ba0f5944a139368bf
Lukario45 <connorcs@ptd.net>
2014-08-09 21:58:06 -0400

    Added Essential Packet Shit

---
+++ src/main/java/net/mcthunder/apis/Packet.java
@@ -0,0 +1,16 @@
+package net.mcthunder.apis;
+
+import java.io.IOException;
+
+/**
+ * Created by Kevin on 8/9/2014.
+ */
+public abstract interface Packet {
+    public abstract void read(NetIn paramNetInput)
+            throws IOException;
+
+    public abstract void write(NetOut paramNetOutput)
+            throws IOException;
+
+    public abstract boolean isPriority();
+}
