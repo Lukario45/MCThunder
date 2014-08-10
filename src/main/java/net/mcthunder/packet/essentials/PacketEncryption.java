ade441efc340962786b8d67d62095057db62f1ee
Lukario45 <connorcs@ptd.net>
2014-08-10 16:19:46 -0400

    essential stuff

---
+++ src/main/java/net/mcthunder/packet/essentials/PacketEncryption.java
@@ -0,0 +1,13 @@
+package net.mcthunder.packet.essentials;
+
+public abstract interface PacketEncryption {
+    public abstract int getDecryptOutputSize(int paramInt);
+
+    public abstract int getEncryptOutputSize(int paramInt);
+
+    public abstract int decrypt(byte[] paramArrayOfByte1, int paramInt1, int paramInt2, byte[] paramArrayOfByte2, int paramInt3)
+            throws Exception;
+
+    public abstract int encrypt(byte[] paramArrayOfByte1, int paramInt1, int paramInt2, byte[] paramArrayOfByte2, int paramInt3)
+            throws Exception;
+}
