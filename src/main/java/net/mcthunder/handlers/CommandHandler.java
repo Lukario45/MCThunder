748d5efb4e7978f33d536bb054f8dad23c8823c5
Lukario45 <connorcs@ptd.net>
2014-10-14 11:26:52 -0400

    updated imports v1

--- src/main/java/net/mcthunder/handlers/CommandHandler.java
+++ src/main/java/net/mcthunder/handlers/CommandHandler.java
@@ -1,12 +1,15 @@
 package net.mcthunder.handlers;
 
+import org.apache.commons.lang.StringUtils;
+import net.mcthunder.apis.Command;
+import net.mcthunder.apis.CommandRegistry;
 /**
  * Created by Kevin on 10/13/2014.
  */
 public class CommandHandler {
 
 
-    public void handleCommand() {
+    public void handleCommand(Server server, Session session, ClientChatPacket packet) {
         String command = StringUtils.lowerCase(packet.getMessage().split(" ")[0].split("/")[1]);
 
         Command cmd = CommandRegistry.getCommand(command, "net.mcthunder.commands.");
