83b1642d7b296d2d1e1f2abc3f35b513e2b596bc
Lukario45 <connorcs@ptd.net>
2014-10-14 11:28:13 -0400

    updated imports again

--- src/main/java/net/mcthunder/handlers/CommandHandler.java
+++ src/main/java/net/mcthunder/handlers/CommandHandler.java
@@ -3,13 +3,17 @@ package net.mcthunder.handlers;
 import org.apache.commons.lang.StringUtils;
 import net.mcthunder.apis.Command;
 import net.mcthunder.apis.CommandRegistry;
+
+import org.spacehq.packetlib.Server;
+import org.spacehq.packetlib.Session;
+import org.spacehq.mc.protocol.packet.ingame.client.ClientChatPacket;
 /**
  * Created by Kevin on 10/13/2014.
  */
 public class CommandHandler {
 
 
-    public void handleCommand(Server server, Session session, ClientChatPacket packet) {
+    public void handlePlayerCommand(Server server, Session session, ClientChatPacket packet) {
         String command = StringUtils.lowerCase(packet.getMessage().split(" ")[0].split("/")[1]);
 
         Command cmd = CommandRegistry.getCommand(command, "net.mcthunder.commands.");
