5b87e7b1491a4968e1f9104934357b23d66186b3
Lukario45 <connorcs@ptd.net>
2014-10-14 11:22:44 -0400

    Create PlayerCommandEventListener.java

---
+++ src/main/java/net/mcthunder/events/listeners/PlayerCommandEventListener.java
@@ -0,0 +1,26 @@
+package net.mcthunder.events.listeners;
+
+//import net.mcthunder.handlers.CommandHandler;
+import org.spacehq.mc.protocol.packet.ingame.client.ClientChatPacket;
+import org.spacehq.packetlib.Server;
+import org.spacehq.packetlib.Session;
+
+import java.util.List;
+
+/**
+ * Created by Kevin on 10/13/2014.
+ */
+public class PlayerChatEventListener implements net.mcthunder.interfaces.PlayerCommandEventListener {
+    //private ServerChatHandler serverChatHandler;
+
+    @Override
+    public boolean removeDefaultListener() {
+        return false;
+    }
+
+    @Override
+    public void onCommand(Server server, Session session, ClientChatPacket packet) {
+        //serverChatHandler = new ServerChatHandler();
+        serverChatHandler.handleChat(server, session, packet, sessionsList);
+    }
+}
