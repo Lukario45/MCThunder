bd9a37447bdcdc24b0412cd6f060bfd9f8128714
Lukario45 <connorcs@ptd.net>
2014-10-14 11:18:53 -0400

    Create PlayerCommandEventSource.java

---
+++ src/main/java/net/mcthunder/events/source/PlayerCommandEventSource.java
@@ -0,0 +1,37 @@
+package net.mcthunder.events.source;
+
+import net.mcthunder.events.PlayerChatEvent;
+import net.mcthunder.interfaces.PlayerChatEventListener;
+import org.spacehq.mc.protocol.packet.ingame.client.ClientChatPacket;
+import org.spacehq.packetlib.Server;
+import org.spacehq.packetlib.Session;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Kevin on 10/13/2014.
+ */
+public class PlayerCommandEventSource {
+    private List playerChatEventListeners = new ArrayList();
+
+    public synchronized void addEventListener(PlayerChatEventListener listener) {
+        if (listener.removeDefaultListener()) {
+            playerChatEventListeners.remove(0);
+        }
+        playerChatEventListeners.add(listener);
+    }
+
+    public synchronized void removeEventListener(PlayerChatEventListener listener) {
+        playerChatEventListeners.remove(listener);
+    }
+
+    public synchronized void fireEvent(Server server, Session session, ClientChatPacket packet, List<Session> sessionsList) {
+        PlayerChatEvent event = new PlayerChatEvent(this);
+        Iterator iterator = playerChatEventListeners.iterator();
+        while (iterator.hasNext()) {
+            ((PlayerChatEventListener) iterator.next()).onChat(server, session, packet, sessionsList);
+        }
+    }
+}
