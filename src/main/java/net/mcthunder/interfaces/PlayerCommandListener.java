3cdb665b2e8eb59b3b28f8980c17aece66538148
Lukario45 <connorcs@ptd.net>
2014-10-14 00:23:43 -0400

    PlayerCommandListener Interface

---
+++ src/main/java/net/mcthunder/interfaces/PlayerCommandListener.java
@@ -0,0 +1,14 @@
+package net.mcthunder.interfaces;
+
+import org.spacehq.mc.protocol.packet.ingame.client.ClientChatPacket;
+import org.spacehq.packetlib.Server;
+import org.spacehq.packetlib.Session;
+
+/**
+ * Created by Kevin on 10/14/2014.
+ */
+public interface PlayerCommandListener {
+    public boolean removeDefaultListener();
+
+    public void onCommand(Server server, Session session, ClientChatPacket packet);
+}
