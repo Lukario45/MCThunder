bc60de02cae4cacc4e1c7b00b5f3320f7335ade3
pupnewfster <pupnewfster@gmail.com>
2014-10-16 20:49:25 -0400

    May or may not fix chat coloring

--- src/main/java/net/mcthunder/handlers/ServerChatHandler.java
+++ src/main/java/net/mcthunder/handlers/ServerChatHandler.java
@@ -31,9 +31,10 @@ public class ServerChatHandler {
             String userName = profile.getName();
             String message = packet.getMessage();
             tellConsole("CHAT", userName + ": " + message);
-            Message msg = new TextMessage(userName).setStyle(new MessageStyle().setColor(ChatColor.YELLOW));
-            Message body = new TextMessage(": " + message).setStyle(new MessageStyle().setColor(ChatColor.WHITE));
+            Message msg = new TextMessage(userName).setStyle(new MessageStyle().setColor(ChatColor.WHITE));
+            Message body = new TextMessage(": " + message);
             msg.addExtra(body);
+            msg.setStyle(new MessageStyle().setColor(ChatColor.YELLOW));
             sessionsList = server.getSessions();
             for (Session s : sessionsList) {
                 s.send(new ServerChatPacket(msg));
