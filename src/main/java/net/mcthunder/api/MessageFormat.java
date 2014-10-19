5f2eaf8aed868d1ab84a7e502f57b12c70aeeda0
pupnewfster <pupnewfster@gmail.com>
2014-10-19 12:42:30 -0400

    Fix things to carry as they should

--- src/main/java/net/mcthunder/api/MessageFormat.java
+++ src/main/java/net/mcthunder/api/MessageFormat.java
@@ -2,6 +2,8 @@ package net.mcthunder.api;
 
 import org.spacehq.mc.protocol.data.message.*;
 
+import java.util.ArrayList;
+
 /**
  * Created by Kevin on 10/18/2014.
  */
@@ -11,79 +13,105 @@ public class MessageFormat {
             return new TextMessage(message);
         String[] brokenMessage = message.split("&");
         Message msg = new TextMessage("");
+        ChatColor prev = ChatColor.WHITE;
+        ArrayList<ChatFormat> formats = new ArrayList<ChatFormat>();
         for (String m : brokenMessage) {
-            if (m.equals(""))
+            if (m.equals("")) {
+                msg.addExtra(new TextMessage("&").setStyle(new MessageStyle().setColor(prev)));
                 continue;
+            }
             Character color = m.charAt(0);
             switch (color) {
                 case 'a':
-                    msg.addExtra(new TextMessage(m.replaceFirst("a", "")).setStyle(new MessageStyle().setColor(ChatColor.GREEN)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("a", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.GREEN)));
                     break;
                 case 'b':
-                    msg.addExtra(new TextMessage(m.replaceFirst("b", "")).setStyle(new MessageStyle().setColor(ChatColor.AQUA)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("b", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.AQUA)));
                     break;
                 case 'c':
-                    msg.addExtra(new TextMessage(m.replaceFirst("c", "")).setStyle(new MessageStyle().setColor(ChatColor.RED)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("c", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.RED)));
                     break;
                 case 'd':
-                    msg.addExtra(new TextMessage(m.replaceFirst("d", "")).setStyle(new MessageStyle().setColor(ChatColor.LIGHT_PURPLE)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("d", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.LIGHT_PURPLE)));
                     break;
                 case 'e':
-                    msg.addExtra(new TextMessage(m.replaceFirst("e", "")).setStyle(new MessageStyle().setColor(ChatColor.YELLOW)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("e", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.YELLOW)));
                     break;
                 case 'f':
-                    msg.addExtra(new TextMessage(m.replaceFirst("f", "")).setStyle(new MessageStyle().setColor(ChatColor.WHITE)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("f", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.WHITE)));
                     break;
                 case '0':
-                    msg.addExtra(new TextMessage(m.replaceFirst("0", "")).setStyle(new MessageStyle().setColor(ChatColor.BLACK)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("0", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.BLACK)));
                     break;
                 case '1':
-                    msg.addExtra(new TextMessage(m.replaceFirst("1", "")).setStyle(new MessageStyle().setColor(ChatColor.DARK_BLUE)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("1", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.DARK_BLUE)));
                     break;
                 case '2':
-                    msg.addExtra(new TextMessage(m.replaceFirst("2", "")).setStyle(new MessageStyle().setColor(ChatColor.DARK_GREEN)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("2", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.DARK_GREEN)));
                     break;
                 case '3':
-                    msg.addExtra(new TextMessage(m.replaceFirst("3", "")).setStyle(new MessageStyle().setColor(ChatColor.DARK_AQUA)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("3", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.DARK_AQUA)));
                     break;
                 case '4':
-                    msg.addExtra(new TextMessage(m.replaceFirst("4", "")).setStyle(new MessageStyle().setColor(ChatColor.DARK_RED)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("4", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.DARK_RED)));
                     break;
                 case '5':
-                    msg.addExtra(new TextMessage(m.replaceFirst("5", "")).setStyle(new MessageStyle().setColor(ChatColor.DARK_PURPLE)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("5", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.DARK_PURPLE)));
                     break;
                 case '6':
-                    msg.addExtra(new TextMessage(m.replaceFirst("6", "")).setStyle(new MessageStyle().setColor(ChatColor.GOLD)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("6", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.GOLD)));
                     break;
                 case '7':
-                    msg.addExtra(new TextMessage(m.replaceFirst("7", "")).setStyle(new MessageStyle().setColor(ChatColor.GRAY)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("7", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.GRAY)));
                     break;
                 case '8':
-                    msg.addExtra(new TextMessage(m.replaceFirst("8", "")).setStyle(new MessageStyle().setColor(ChatColor.DARK_GRAY)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("8", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.DARK_GRAY)));
                     break;
                 case '9':
-                    msg.addExtra(new TextMessage(m.replaceFirst("9", "")).setStyle(new MessageStyle().setColor(ChatColor.BLUE)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("9", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.BLUE)));
                     break;
                 case 'l':
-                    msg.addExtra(new TextMessage(m.replaceFirst("l", "")).setStyle(new MessageStyle().addFormat(ChatFormat.BOLD)));
+                    formats.add(ChatFormat.BOLD);
+                    msg.addExtra(new TextMessage(m.replaceFirst("l", "")).setStyle(new MessageStyle().setFormats(formats)));
                     break;
                 case 'n':
-                    msg.addExtra(new TextMessage(m.replaceFirst("n", "")).setStyle(new MessageStyle().addFormat(ChatFormat.UNDERLINED)));
+                    formats.add(ChatFormat.UNDERLINED);
+                    msg.addExtra(new TextMessage(m.replaceFirst("n", "")).setStyle(new MessageStyle().setColor(prev).setFormats(formats)));
                     break;
                 case 'o':
-                    msg.addExtra(new TextMessage(m.replaceFirst("o", "")).setStyle(new MessageStyle().addFormat(ChatFormat.ITALIC)));
+                    formats.add(ChatFormat.ITALIC);
+                    msg.addExtra(new TextMessage(m.replaceFirst("o", "")).setStyle(new MessageStyle().setColor(prev).setFormats(formats)));
                     break;
                 case 'k':
-                    msg.addExtra(new TextMessage(m.replaceFirst("k", "")).setStyle(new MessageStyle().addFormat(ChatFormat.OBFUSCATED)));
+                    formats.add(ChatFormat.OBFUSCATED);
+                    msg.addExtra(new TextMessage(m.replaceFirst("k", "")).setStyle(new MessageStyle().setColor(prev).setFormats(formats).setFormats(formats)));
                     break;
                 case 'm':
-                    msg.addExtra(new TextMessage(m.replaceFirst("m", "")).setStyle(new MessageStyle().addFormat(ChatFormat.STRIKETHROUGH)));
+                    formats.add(ChatFormat.STRIKETHROUGH);
+                    msg.addExtra(new TextMessage(m.replaceFirst("m", "")).setStyle(new MessageStyle().setColor(prev).setFormats(formats)));
                     break;
                 case 'r':
-                    msg.addExtra(new TextMessage(m.replaceFirst("r", "")).setStyle(new MessageStyle().setColor(ChatColor.RESET)));
+                    formats = new ArrayList<ChatFormat>();
+                    msg.addExtra(new TextMessage(m.replaceFirst("r", "")).setStyle(new MessageStyle().setColor(prev = ChatColor.RESET)));
                     break;
                 default:
-                    msg.addExtra(new TextMessage("&" + m).setStyle(new MessageStyle().setColor(ChatColor.WHITE)));
+                    msg.addExtra(new TextMessage("&" + m).setStyle(new MessageStyle().setColor(prev)));
                     break;
             }
         }
