15f1bd1a289a6cf70379b3f8393447bc486fc200
Lukario45 <lukario45@mcthunder.net>
2015-03-29 16:58:26 -0400

    Default Rank Command

--- src/main/java/net/mcthunder/rankmanager/commands/SetDefaultRank.java
+++ src/main/java/net/mcthunder/rankmanager/commands/SetDefaultRank.java
@@ -1,7 +1,14 @@
 package net.mcthunder.rankmanager.commands;
 
+import com.Lukario45.NBTFile.Utilities;
+import net.mcthunder.MCThunder;
 import net.mcthunder.api.Command;
+import net.mcthunder.api.LoggingLevel;
 import net.mcthunder.entity.Player;
+
+import java.io.IOException;
+
+import static net.mcthunder.api.Utils.tellConsole;
 
 /**
  * Created by conno_000 on 3/29/2015.
@@ -13,6 +20,24 @@ public class SetDefaultRank extends Command {
 
     @Override
     public boolean execute(Player player, String[] args) {
-        return false;
+        if (args.length < 1){
+            return false;
+        } else {
+            if  (!MCThunder.getRankManager().getRankHashMap().containsKey(args[0])){
+                player.sendMessage("&cThat rank does not exist yet!");
+                return true;
+            } else {
+                try {
+                    MCThunder.getRankManager().getRanks().write(Utilities.makeStringTag("DefaultRank", args[0]));
+                    player.sendMessage("&bDefault rank has been changed to " + args[0] + "!");
+                    tellConsole(LoggingLevel.INFO,"&bDefault rank has been changed to " + args[0] + "!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+
+
+        }
     }
 }
