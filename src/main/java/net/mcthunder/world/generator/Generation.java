6a14a22d954bb5e64db40f755dc011b57cfd6c4e
Connor Smith <connorcs@ptd.net>
2015-05-28 10:38:08 -0400

    Notes

--- src/main/java/net/mcthunder/world/generator/Generation.java
+++ src/main/java/net/mcthunder/world/generator/Generation.java
@@ -54,3 +54,120 @@ public class Generation {
         //chunks[i] = chunk;
     }
 }
+/**
+ * THESE ARE NOTES ON 3D PERLIN NOISE FOR WORLD GENERATION
+ * I UNDERSTAND HOW IT WORKS SORT OF BUT I DO NOT KNOW THE LOGIC BEHIND PERLIN NOISE 3D
+ * http://devmag.org.za/2009/04/25/perlin-noise/
+ * Must create number of smooth noise arrays
+ * Each array is called an octave
+ * Blend all together 
+ * Create "Smooth Noise" float between 0 and One
+ * This is Direct Code from the website 
+ * float[][] GenerateWhiteNoise(int width, int height)
+{
+    Random random = new Random(0); //Seed to 0 for testing
+    float[][] noise = GetEmptyArray(width, height);
+ 
+    for (int i = 0; i &lt; width; i++)
+    {
+        for (int j = 0; j &lt; height; j++)
+        {
+            noise[i][j] = (float)random.NextDouble() % 1;
+        }
+    }
+ 
+    return noise;
+}
+* Direct quote
+* "For creating the kth octave, sample the noise array at every point (i*2k, j*2k) , for all i, j, and interpolate 
+* the other points linearly. The value 2k is called the wave length of that octave, and the value 1/2k is called the frequency.
+
+* The following pseudo C snippet shows how the kth octave is generated. Index variables are integers,
+* and remember that integer division gives a floored integer result (for example, 5 / 3 gives 1)."
+* Some more code
+float[][] GenerateSmoothNoise(float[][] baseNoise, int octave)
+{
+   int width = baseNoise.Length;
+   int height = baseNoise[0].Length;
+ 
+   float[][] smoothNoise = GetEmptyArray(width, height);
+ 
+   int samplePeriod = 1 &lt;&lt; octave; // calculates 2 ^ k
+   float sampleFrequency = 1.0f / samplePeriod;
+ 
+   for (int i = 0; i &lt; width; i++)
+   {
+      //calculate the horizontal sampling indices
+      int sample_i0 = (i / samplePeriod) * samplePeriod;
+      int sample_i1 = (sample_i0 + samplePeriod) % width; //wrap around
+      float horizontal_blend = (i - sample_i0) * sampleFrequency;
+ 
+      for (int j = 0; j &lt; height; j++)
+      {
+         //calculate the vertical sampling indices
+         int sample_j0 = (j / samplePeriod) * samplePeriod;
+         int sample_j1 = (sample_j0 + samplePeriod) % height; //wrap around
+         float vertical_blend = (j - sample_j0) * sampleFrequency;
+ 
+         //blend the top two corners
+         float top = Interpolate(baseNoise[sample_i0][sample_j0],
+            baseNoise[sample_i1][sample_j0], horizontal_blend);
+ 
+         //blend the bottom two corners
+         float bottom = Interpolate(baseNoise[sample_i0][sample_j1],
+            baseNoise[sample_i1][sample_j1], horizontal_blend);
+ 
+         //final blend
+         smoothNoise[i][j] = Interpolate(top, bottom, vertical_blend);
+      }
+   }
+ 
+   return smoothNoise;
+}
+* Generate Perlin Noise code
+float[][] GeneratePerlinNoise(float[][] baseNoise, int octaveCount)
+{
+   int width = baseNoise.Length;
+   int height = baseNoise[0].Length;
+ 
+   float[][][] smoothNoise = new float[octaveCount][][]; //an array of 2D arrays containing
+ 
+   float persistance = 0.5f;
+ 
+   //generate smooth noise
+   for (int i = 0; i &lt; octaveCount; i++)
+   {
+       smoothNoise[i] = GenerateSmoothNoise(baseNoise, i);
+   }
+ 
+    float[][] perlinNoise = GetEmptyArray(width, height);
+    float amplitude = 1.0f;
+    float totalAmplitude = 0.0f;
+ 
+    //blend noise together
+    for (int octave = octaveCount - 1; octave &gt;= 0; octave--)
+    {
+       amplitude *= persistance;
+       totalAmplitude += amplitude;
+ 
+       for (int i = 0; i &lt; width; i++)
+       {
+          for (int j = 0; j &lt; height; j++)
+          {
+             perlinNoise[i][j] += smoothNoise[octave][i][j] * amplitude;
+          }
+       }
+    }
+ 
+   //normalisation
+   for (int i = 0; i &lt; width; i++)
+   {
+      for (int j = 0; j &lt; height; j++)
+      {
+         perlinNoise[i][j] /= totalAmplitude;
+      }
+   }
+ 
+   return perlinNoise;
+}
+* */
