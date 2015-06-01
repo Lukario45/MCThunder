8960ed06f67e6cb3e236fa747fa7db0342bb16e0
Connor Smith <connorcs@ptd.net>
2015-06-01 10:37:39 -0400

    Update Broadcast.java

--- src/main/java/net/mcthunder/commands/Broadcast.java
+++ src/main/java/net/mcthunder/commands/Broadcast.java
@@ -1,1 +1,18 @@
 package net.mcthunder.commands
+
+import net.mcthunder.MCThunder;
+import net.mcthunder.api.Command;
+
+public class Broadcast extends Command {
+  public Broadcast(){
+    super("broadcast", Arrays.asList("say"), "Broadcast a sever message", "/broadcast {message}", 5000, "core.broadcast");
+  }
+  
+  @Override
+    public boolean execute(Player player, String[] args) {
+        if (args.length == 0)
+            return false;
+        
+        return true;
+    }
+}
