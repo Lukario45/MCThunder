02954ac6a15af3d03f86d82d63fc4f1d3b18b9c4
Lukario45 <lukario45@mcthunder.net>
2015-06-06 21:59:35 -0400

    Plugin Abstract

---
+++ src/main/java/net/mcthunder/net/mcthunder/plugin/MCThunderPlugin.java
@@ -0,0 +1,37 @@
+package net.mcthunder.net.mcthunder.plugin;
+
+import java.util.List;
+
+/**
+ * Created by conno_000 on 6/6/2015.
+ */
+public abstract class MCThunderPlugin {
+   private final String pluginName;
+   private final String version;
+   private final List<String> developers;
+   private final String commandPath;
+
+   public MCThunderPlugin(String pluginName, String version, List<String> developers, String commandPath){
+      this.pluginName = pluginName;
+      this.version = version;
+      this.developers = developers;
+      this.commandPath = commandPath;
+   }
+
+   public abstract boolean load();
+
+   public String getPluginName(){
+      return this.pluginName;
+   }
+   public String getVersion(){
+      return this.version;
+   }
+   public List<String> getDevelopers(){
+      return this.developers;
+   }
+   public String getCommandPath(){
+      return this.commandPath;
+   }
+
+
+}
